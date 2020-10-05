51378f4bbefac8e250a0abfb9ca835249fd6d46c
ginarasillo <70277129+ginarasillo@users.noreply.github.com>
2020-10-05 17:50:42 -0500

    python_challenge

---
+++ PyBank/main.py
@@ -0,0 +1,67 @@
+
+
+import csv
+
+csvpath = "budget_data.csv"
+
+months= []
+total= 0
+total_change = 0
+last_value = None
+g_increase= 0
+g_decrease= 0
+increase_month= ""
+decrease_month= ""
+
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first 
+    # (skip this step if there is now header)
+    csv_header = next(csvreader)
+    
+
+    # Read each row of data after the header
+    for row in csvreader:
+
+        months.append(row[0])
+        profit = float(row[1])
+        total += profit
+        if last_value:
+            change = profit - last_value
+            total_change += change
+            if change > g_increase:
+                g_increase= change
+                increase_month= row[0]
+            if change < g_decrease:
+                g_decrease=change
+                decrease_month= row[0]
+
+        last_value = profit
+
+        
+
+
+
+        # si profit es mayor a mi greatest
+            # mi nuevo greatest es profit
+            # mi nuevo greatest_month es month
+
+unique_months = set(months)
+
+
+result= f"""
+Financial Analysis
+-----------------------
+Total Months: {len(unique_months)}
+Total: ${total}
+Average Change: {total_change/(len(months)-1):.2f}
+Greatest Increase in Profits: {increase_month} (${g_increase})
+Greatest Decrease in Profits: {decrease_month} (${g_decrease})
+"""
+
+print(result)
+
+with open("budget_result.txt", "w") as result_f:
+    result_f.write(result)
---
+++ PyPoll/main.py
@@ -0,0 +1,54 @@
+import csv
+
+csvpath = "election_data.csv"
+
+votes= []
+candidates= {}
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    csv_header = next(csvreader)
+    
+
+    for row in csvreader:
+
+        votes.append(row[0])
+        voted_candidate= row[2]
+        if voted_candidate in candidates:
+            candidates[voted_candidate] += 1
+        else:
+            candidates[voted_candidate] = 1
+unique_votes = set(votes)
+
+winner_votes= 0
+winner = ""
+candidate_result = ""
+for candidate, c_votes in candidates.items():
+    percentage = c_votes/len(unique_votes)*100.0
+    if c_votes > winner_votes:
+        winner= candidate
+        winner_votes= c_votes
+    candidate_result += f"{candidate} %{percentage:.2f} ({c_votes})\n"
+
+
+
+result= f"""
+Election Results
+-----------------------
+Total votes: {len(unique_votes)}
+-----------------------
+{candidate_result}
+-----------------------
+Winner: {winner}
+"""
+
+
+print(result) 
+
+
+
+with open("election_data.txt", "w") as result_f:
+    result_f.write(result)
+
+
